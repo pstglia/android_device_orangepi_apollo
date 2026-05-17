f3fb58114f49a019765d834015e151373aadd116
Paulo Sergio Travaglia <pstglia@gmail.com>
2026-05-17 00:04:19 -0300

    device/orangepi: WIP
    Uses longan kernel modules by copying them using
    PRODDUCT_COPY_FILES to vendor/lib/modules_longan
    and changes .rc files to use this path instead
    vendor/lib/modules
    
    The original goal was trying to build kernel under
    lineage build, but longan kernel build has some obscure
    signing (like buildserver) that couldn't be ported
    
    Change-Id: If2ea7acbae3d1e556a95302d28020fec81833477

--- device.mk
+++ device.mk
@@ -32,6 +32,44 @@ PRODUCT_COPY_FILES += \
 #PRODUCT_COPY_FILES += \
 #    $(LOCAL_PATH)/vendor/dummy_create_vendor_dir.txt:root/vendor/dsp/dummy_create_vendor_dir.txt
 
+# Copy mali and dma buf kernel modules from longan
+PRODUCT_COPY_FILES += \
+    $(PRODUCT_PREBUILT_PATH)/dist/aic8800_bsp.ko:vendor/lib/modules_longan/aic8800_bsp.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/aic8800_btlpm.ko:vendor/lib/modules_longan/aic8800_btlpm.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/aic8800_fdrv.ko:vendor/lib/modules_longan/aic8800_fdrv.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/arc4.ko:vendor/lib/modules_longan/arc4.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/auth_rpcgss.ko:vendor/lib/modules_longan/auth_rpcgss.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/bcm_btlpm.ko:vendor/lib/modules_longan/bcm_btlpm.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/bcmdhd.ko:vendor/lib/modules_longan/bcmdhd.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/dma-buf-test-exporter.ko:vendor/lib/modules_longan/dma-buf-test-exporter.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/ehci-sunxi.ko:vendor/lib/modules_longan/ehci-sunxi.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/gspca_main.ko:vendor/lib/modules_longan/gspca_main.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/init-input.ko:vendor/lib/modules_longan/init-input.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/mali_kbase.ko:vendor/lib/modules_longan/mali_kbase.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.alias:vendor/lib/modules_longan/modules.alias \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.alias.bin:vendor/lib/modules_longan/modules.alias.bin \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.builtin:vendor/lib/modules_longan/modules.builtin \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.builtin.alias.bin:vendor/lib/modules_longan/modules.builtin.alias.bin \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.builtin.bin:vendor/lib/modules_longan/modules.builtin.bin \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.dep:vendor/lib/modules_longan/modules.dep \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.dep.bin:vendor/lib/modules_longan/modules.dep.bin \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.devname:vendor/lib/modules_longan/modules.devname \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.order:vendor/lib/modules_longan/modules.order \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.softdep:vendor/lib/modules_longan/modules.softdep \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.symbols:vendor/lib/modules_longan/modules.symbols \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.symbols.bin:vendor/lib/modules_longan/modules.symbols.bin \
+    $(PRODUCT_PREBUILT_PATH)/dist/modules.weakdep:vendor/lib/modules_longan/modules.weakdep \
+    $(PRODUCT_PREBUILT_PATH)/dist/Module.symvers:vendor/lib/modules_longan/Module.symvers \
+    $(PRODUCT_PREBUILT_PATH)/dist/ohci-sunxi.ko:vendor/lib/modules_longan/ohci-sunxi.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/rpcsec_gss_krb5.ko:vendor/lib/modules_longan/rpcsec_gss_krb5.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/sprdbt_tty.ko:vendor/lib/modules_longan/sprdbt_tty.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/sprdwl_ng.ko:vendor/lib/modules_longan/sprdwl_ng.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/sunxi-hci.ko:vendor/lib/modules_longan/sunxi-hci.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/sunxi_rfkill.ko:vendor/lib/modules_longan/sunxi_rfkill.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/sunxi_usbc.ko:vendor/lib/modules_longan/sunxi_usbc.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/uwe5622_bsp_sdio.ko:vendor/lib/modules_longan/uwe5622_bsp_sdio.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/wireless_switch.ko:vendor/lib/modules_longan/wireless_switch.ko \
+    $(PRODUCT_PREBUILT_PATH)/dist/xr819.ko:vendor/lib/modules_longan/xr819.ko
 
 # wifi and bt configuration
 # 1. Wifi Configuration
